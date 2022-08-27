c4040f2859c3c0a831f9fb2de0fe4cd00539f847
kche1106 <kche011106@gmail.com>
2022-08-27 17:48:17 +0900

    데이터 타입

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,11 @@
+var message : String
+
+//자료형 명시안해도 저장되는 데이터 타입을 보고 내부적으로 타입 추론해 데이터 타입 저장
+var isLogin = false
+
+var str: String = "Hello~"
+var day: Int = 27
+var weight: Double = 65.5
+
+//다른 데이터타입은 저장 불가
+//message = 17
