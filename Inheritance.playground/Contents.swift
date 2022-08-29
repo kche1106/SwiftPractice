d67a21287001064e5e604b4aa94cf64cc1b35cfe
kche1106 <kche011106@gmail.com>
2022-08-29 18:38:17 +0900

    오버라이드

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -10,6 +10,11 @@ class MacBook: NoteBook {
     var touchBar = true
     var OSVersion = "10"
     
+    override func turnOn() {
+        super.turnOn()  //super class의 인스턴스
+        print("macBook Booting..")
+    }
+    
     func turnOnTouchBar() {
         
     }
