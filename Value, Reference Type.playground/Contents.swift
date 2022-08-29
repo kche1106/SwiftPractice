a98cffa3e0fa8ff81c2b1eedda52ae39d1ccb1fa
kche1106 <kche011106@gmail.com>
2022-08-29 18:46:22 +0900

    Value, Reference

---
+++ Value, Reference Type.playground/Contents.swift
@@ -0,0 +1,13 @@
+struct Circle {
+    var radius = 0
+}
+
+var circle1 = Circle()
+var circle2 = circle1
+circle2.radius = 10
+
+print(circle2.radius)
+print(circle1.radius)
+
+//Circle이 class인 경우 circle1의 radius값도 10으로 바뀜 (참조하는 메모리가 동일하므로)
+//Circle이 구조체인 경우 circle2가 가리키는 또다른 인스턴스가 생김, circle1에 영향 X
