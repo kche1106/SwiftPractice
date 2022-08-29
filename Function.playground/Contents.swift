bee81806837a2ec07673456f04c80ca3e9d701b2
kche1106 <kche011106@gmail.com>
2022-08-29 16:34:52 +0900

    함수의 다양한 형태

--- Function.playground/Contents.swift
+++ Function.playground/Contents.swift
@@ -10,9 +10,35 @@ print(message)
 print("HI")  //print 함수
 
 
-
-
+//코드 스니펫
 func addTwoNumbers(num1: Int, num2: Int) -> Int {
     let sum = num1 + num2
     return sum
 }
+ addTwoNumbers(num1: 5, num2: 10)
+
+
+func addTwoNumbers2(num1: Int, num2: Int = 100) -> Int {
+    let sum = num1 + num2
+    return sum
+}
+addTwoNumbers2(num1: 5)
+
+
+func addNumbers(numbers: Int...) -> Int {
+    var sum = 0
+    for num in numbers {
+        sum += num
+    }
+    return sum
+}
+addNumbers(numbers: 1, 2, 3, 4, 5)
+
+
+func myInfo() -> (name: String, weight: Int) {
+    return ("채은", 40)
+}
+
+let info = myInfo()
+print(info.name)
+print(info)
