cacc9fb1108aab71e1a5e8bc21c1e6283e392f51
kche1106 <kche011106@gmail.com>
2022-08-29 16:24:42 +0900

    함수

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,10 @@
+func hello(name: String) -> String {
+    let message = "Hello " + name
+    return message
+    //return "Hello " + name
+}
+
+let message = hello(name: "채은")
+print(message)
+
+print("HI")  //print 함수
